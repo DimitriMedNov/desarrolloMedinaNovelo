a7fec41a296edae54641f53a7b3fc1d8d3bc050b
Dimitri Medina <00396910@anahuac.mx>
2023-01-20 16:40:00 -0600

    Clase Controller

---
+++ src/main/java/com/mayab/desarrollo/creacional/singleton/BD_Connect.java
@@ -0,0 +1,20 @@
+package com.mayab.desarrollo.creacional.singleton;
+
+public class BD_Connect {
+    //Singleton start //Nunca cambia
+        private static BD_Connect instance = new BD_Connect();
+        private BD_Connect(){  }
+        public static BD_Connect getInstance(){
+            if (BD_Connect.instance == null){
+                BD_Connect.instance = new BD_Connect();
+            }
+            return BD_Connect.instance;
+        }
+    //Singleton end
+
+    //Metodo para tener la conecxion
+    public void getConnection(){
+        System.out.println("Ya se conecto\n");
+    }
+
+}
