0c4d79c194c6d559182d2d8f2764634a62196528
Dimitri Medina <00396910@anahuac.mx>
2023-01-22 22:48:05 -0600

    Edite el texto que muestran los metodos Print de los reportes

--- src/main/java/com/mayab/desarrollo/creacional/singleton/Controller.java
+++ src/main/java/com/mayab/desarrollo/creacional/singleton/Controller.java
@@ -4,8 +4,8 @@ public class Controller {
     public static void main( String[] args )
     {
         //Instancie los objetos de las clases Reportes A/B
-        Reporte_A ReporteA = new Reporte_A("Se realizo la conecxion com el Reporte_A, satisfactoriamente!");
-        Reporte_B ReporteB = new Reporte_B("Se realizo la conecxion com el Reporte _B, satisfactoriamente!");
+        Reporte_A ReporteA = new Reporte_A("Se ha realizado la conecxion com el Reporte A, satisfactoriamente!");
+        Reporte_B ReporteB = new Reporte_B("Se ha realizado la conecxion com el Reporte B, satisfactoriamente!");
 
         //LLamo a los metodos Print de los Reportes A y B
         ReporteA.print();
